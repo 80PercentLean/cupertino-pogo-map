c247187565969b38fe06aef448d33ea357e3be3d
Lean <80percentlean@gmail.com>
2025-07-15 15:43:07 -0700

    Setup Stylelint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import stylelint from 'vite-plugin-stylelint'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), stylelint()],
 })
