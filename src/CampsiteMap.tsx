4f8fee8a2bf7f2a162f0497a2c35a8ca1a7ef056
Lean <80percentlean@gmail.com>
2025-07-15 17:00:19 -0700

    Make first changes to CampsiteMap - Set starting coordinates to Cupertino PoGO Group's Community Campsite - Conditionally disable scroll wheel zoom when using mobile browser

--- src/CampsiteMap.tsx
+++ src/CampsiteMap.tsx
@@ -1,18 +1,27 @@
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
-export default function BasicMap() {
+import { isMobile } from './util'
+
+const IS_MOBILE = isMobile()
+
+/**
+ * The map of the Campsite.
+ *
+ * @returns A React Leaflet component
+ */
+export default function CampsiteMap() {
   return (
     <MapContainer
       id="map"
-      center={[51.505, -0.09]}
+      center={[37.324839617734014, -122.04457760463976]}
       zoom={17}
-      scrollWheelZoom={false}
+      scrollWheelZoom={!IS_MOBILE}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[37.324839617734014, -122.04457760463976]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
---
+++ src/util.ts
@@ -0,0 +1,23 @@
+/**
+ * Checks if the browser is running on a mobile device or not.
+ * Original code taken from: https://stackoverflow.com/questions/77506413/detecting-if-the-user-is-on-desktop-or-mobile-in-the-browser
+ *
+ * @returns {Boolean} True if the user is detected to be using a mobile browser, false if not
+ */
+export const isMobile = () => {
+  console.log('User agent:', navigator.userAgent)
+  const result =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent,
+    )
+
+  if (result) {
+    // User is accessing the page on a mobile device
+    console.log('Mobile device detected')
+  } else {
+    // User is accessing the page on a desktop device
+    console.log('Desktop device detected')
+  }
+
+  return result
+}
