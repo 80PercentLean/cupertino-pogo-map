5e190b916df09babacda0609eb3313e8cb03ef1f
Lean <80percentlean@gmail.com>
2025-10-28 02:04:51 -0700

    Fix marker icon not being centered properly & add circle around live location

--- reset/src/LiveLocationMarker.tsx
+++ reset/src/LiveLocationMarker.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
-import type { LatLngExpression } from "leaflet";
+import { Circle } from "react-leaflet";
+import { type LatLngExpression } from "leaflet";
 import CMarker, { type CMarkerProps } from "./CMarker";
 
 export type LiveLocationMarkerProps = Omit<CMarkerProps, "position">;
 
 export default function LiveLocationMarker(props: LiveLocationMarkerProps) {
   const [location, setLocation] = useState<LatLngExpression | null>(null);
+  const [accuracy, setAccuracy] = useState<number | null>(null);
 
   useEffect(() => {
     let watchId: number | null = null;
@@ -17,8 +19,10 @@ export default function LiveLocationMarker(props: LiveLocationMarkerProps) {
             "GPS coordinates",
             position.coords.latitude,
             position.coords.longitude,
+            position.coords.accuracy,
           );
           setLocation([position.coords.latitude, position.coords.longitude]);
+          setAccuracy(position.coords.accuracy);
         },
         (error) => {
           console.log(`ERROR(${error.code}): ${error.message}`);
@@ -34,7 +38,16 @@ export default function LiveLocationMarker(props: LiveLocationMarkerProps) {
   }, []);
 
   if (location) {
-    return <CMarker {...props} position={location} />;
+    let accuracyCircle = null;
+    if (accuracy) {
+      accuracyCircle = <Circle center={location} radius={accuracy} />;
+    }
+    return (
+      <>
+        {accuracyCircle}
+        <CMarker {...props} position={location} />
+      </>
+    );
   }
   return null;
 }
--- reset/src/setupLeaflet.ts
+++ reset/src/setupLeaflet.ts
@@ -4,9 +4,5 @@ import L from "leaflet";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
-let DefaultIcon = L.icon({
-  iconUrl: icon,
-  shadowUrl: iconShadow,
-});
-
-L.Marker.prototype.options.icon = DefaultIcon;
+L.Icon.Default.prototype.options.iconUrl = icon;
+L.Icon.Default.prototype.options.shadowUrl = iconShadow;
