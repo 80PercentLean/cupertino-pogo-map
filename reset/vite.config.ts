e46ba77f28873f2b62d1359e9a849b634efb3f56
Lean <80percentlean@gmail.com>
2025-10-28 21:45:06 -0700

    Enable dev server on local network

--- reset/vite.config.ts
+++ reset/vite.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "cupertino-pogo-map",
+  base: "/cupertino-pogo-map",
+  server: { host: "0.0.0.0" },
   plugins: [
     react({
       babel: {
