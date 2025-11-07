4d31b4f1a036813e1789072a65f40a81b03e77ef
Lean <80percentlean@gmail.com>
2025-11-07 13:37:02 -0800

    - Fix bug where incorrect restrooms were loaded - Setup stylelint

--- src/geojson/data.ts
+++ src/geojson/data.ts
@@ -6,10 +6,10 @@ import l13Cells from "./l13.geojson?raw";
 import l14Cells from "./l14.geojson?raw";
 import l17Cells from "./l17.geojson?raw";
 import labels from "./labels.geojson?raw";
-import restrooms from "./labels.geojson?raw";
 import parking from "./parking.geojson?raw";
 import pokestops from "./pokestops.geojson?raw";
 import powerSpots from "./powerspots.geojson?raw";
+import restrooms from "./restrooms.geojson?raw";
 
 export const devpoisJson = JSON.parse(devpois) as GeoJSON;
 export const gymsJson = JSON.parse(gyms) as GeoJSON;
