a5f60fcd0493a2826cced4df8fdd781387f0ae13
Lean <80percentlean@gmail.com>
2026-01-21 02:10:14 -0800

    - Update DevPois to match new implementation used by all other POIs - Add new Ambassador POIs as dev POIs - Fix incorrect coordinates for De Anza College Book Store PokeStop - Add new Power Spots

--- src/components/poi/DevPois.tsx
+++ src/components/poi/DevPois.tsx
@@ -1,30 +1,49 @@
-import L from "leaflet";
+import { type LatLngTuple } from "leaflet";
+import { Fragment } from "react";
+import { Marker, Popup } from "react-leaflet";
 
 import { devpoisJson } from "../../geojson/data";
 import { iconDev } from "../../leafletIcons";
-import type { CFeature } from "../../types";
-import Poi from "./Poi";
-import { genPopupContent } from "./helper";
+import { useStore } from "../hooks/store";
+import InteractionRadius from "./InteractionRadius";
+import NoCaPoiZone from "./NoCaPoiZone";
+import NoPowerSpotZone from "./NoPowerSpotZone";
+import { genPopupContentReact } from "./helper";
 
 /**
- * Specialized <Poi> for rendering POIs in development like Wayfarer submissions.
+ * Render POIs in development like Wayfarer submissions or Campsite POI proposals.
  */
 export default function DevPois() {
-  return (
-    <Poi
-      data={devpoisJson}
-      pointToLayer={({ properties }, latlng) => {
-        const { name } = properties as CFeature["properties"];
+  const showInteractionRadius = useStore((s) => s.layers.interactionRadii);
+  const showNoCaPoiZones = useStore((s) => s.layers.noCaPoiZones);
+  const showPowerSpotZones = useStore((s) => s.layers.noPowerSpotZones);
+  const wayfarerMode = useStore((s) => s.wayfarerMode);
 
-        return L.marker(latlng, { icon: iconDev }).bindPopup(
-          genPopupContent(
-            name,
-            "Wayfarer Submission",
-            latlng,
-            "<p>This POI is currently going through the Wayfarer submission process. It can potentially become an in-game structure like a PokéStop or power spot.</p>",
-          ),
-        );
-      }}
-    />
-  );
+  return devpoisJson.features.map(({ geometry, properties }, i) => {
+    const latlng = [
+      geometry.coordinates[1],
+      geometry.coordinates[0],
+    ] as LatLngTuple;
+    const { desc, name, type } = properties;
+
+    return (
+      <Fragment key={i}>
+        {showNoCaPoiZones && <NoCaPoiZone latlng={latlng} />}
+        {showInteractionRadius && <InteractionRadius latlng={latlng} />}
+        {showPowerSpotZones && <NoPowerSpotZone latlng={latlng} />}
+        <Marker icon={iconDev} position={latlng}>
+          <Popup>
+            {genPopupContentReact(
+              name,
+              type,
+              latlng,
+              desc,
+              undefined,
+              wayfarerMode,
+            )}
+          </Popup>
+        </Marker>
+      </Fragment>
+    );
+  });
 }
