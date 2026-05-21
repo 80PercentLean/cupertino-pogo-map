3021176a6d7b5f2e4d8f34237136e37b9813e5d6
Lean <80percentlean@gmail.com>
2026-05-20 17:18:48 -0700

    Create custom Vite plugin to switch between HTML titles

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,41 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig, loadEnv } from "vite";
+import { type Plugin, defineConfig, loadEnv } from "vite";
 import stylelint from "vite-plugin-stylelint";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+  /**
+   * Custom Vite plugin that changes the titles of the HTML files depending on conditions.
+   */
+  const htmlTitlePlugin: () => Plugin = () => {
+    return {
+      name: "html-transform",
+      transformIndexHtml(html, ctx) {
+        if (ctx.filename.endsWith("/index.html")) {
+          return html.replace(
+            "__TITLE__",
+            env.VITE_IS_CENTRAL === "true"
+              ? "Wild Goose | Pokémon GO Community in Santa Clara, California"
+              : "Cupertino PoGO | Pokémon GO Community in Cupertino, California",
+          );
+        }
+
+        if (ctx.filename.endsWith("/map.html")) {
+          return html.replace(
+            "__TITLE__",
+            env.VITE_IS_CENTRAL === "true"
+              ? "Wild Goose Map | Directions & Free Parking for Pokémon GO at Santa Clara Central Park"
+              : "Cupertino PoGO Map | Directions & Free Parking for Pokémon GO at Memorial Park & De Anza College",
+          );
+        }
+
+        return html;
+      },
+    };
+  };
 
   return {
     base: env.VITE_ROOT_PATH ?? "/",
@@ -23,6 +52,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     plugins: [
+      htmlTitlePlugin(),
       react({
         babel: {
           plugins: ["babel-plugin-react-compiler"],
