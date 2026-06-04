eb3751a64e0aaf9195708077b3d0d24c1c60da18
Lean <80percentlean@gmail.com>
2026-06-04 11:48:42 -0700

    Finish <SettingsView> tests

--- src/components/__tests__/SettingsView.test.tsx
+++ src/components/__tests__/SettingsView.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import userEvent, { type UserEvent } from "@testing-library/user-event";
 import { MemoryRouter } from "react-router";
 
 import SettingsView from "../SettingsView";
@@ -13,13 +13,48 @@ function TestComponent() {
   );
 }
 
+const expectWayfarerModeToTurnedOn = async (user: UserEvent) => {
+  // Check that the Wayfarer section is not visible
+  expect(
+    screen.queryByRole("group", { name: /Wayfarer Settings/i }),
+  ).not.toBeInTheDocument();
+
+  const switchEle = screen.getByRole("switch", {
+    name: /Enable Wayfarer Mode/i,
+  });
+  expect(switchEle).toBeInTheDocument();
+  expect(switchEle).not.toBeChecked();
+
+  expect(useStore.getState().wayfarerMode).toBe(false);
+  expect(localStorage.getItem("wayfarerMode")).toBe(null);
+
+  // Click the switch to turn on the switch
+  await user.click(switchEle);
+
+  expect(switchEle).toBeChecked();
+
+  // Check that the Wayfarer section is visible
+  expect(
+    screen.getByRole("group", { name: /Wayfarer Settings/i }),
+  ).toBeInTheDocument();
+
+  expect(useStore.getState().wayfarerMode).toBe(true);
+  expect(localStorage.getItem("wayfarerMode")).toBe("true");
+
+  return switchEle;
+};
+
+afterEach(() => {
+  localStorage.clear();
+});
+
 test("loads <SettingsView>", () => {
   render(<TestComponent />);
 
   expect(screen.getAllByText(/Settings/i)).toHaveLength(2);
 });
 
-test("enables disable animations switch when switch is clicked", async () => {
+test("enables disable animations when switch is clicked", async () => {
   const user = userEvent.setup();
 
   render(<TestComponent />);
@@ -29,12 +64,16 @@ test("enables disable animations switch when switch is clicked", async () => {
   });
   expect(switchEle).toBeInTheDocument();
   expect(switchEle).not.toBeChecked();
+
   expect(useStore.getState().disableAnimations).toBe(false);
+  expect(localStorage.getItem("disableAnimations")).toBe(null);
 
   await user.click(switchEle);
 
   expect(switchEle).toBeChecked();
+
   expect(useStore.getState().disableAnimations).toBe(true);
+  expect(localStorage.getItem("disableAnimations")).toBe("true");
 });
 
 test("enables simple markers when switch is clicked", async () => {
@@ -47,30 +86,16 @@ test("enables simple markers when switch is clicked", async () => {
   });
   expect(switchEle).toBeInTheDocument();
   expect(switchEle).not.toBeChecked();
+
   expect(useStore.getState().isSimpleMarkerEnabled).toBe(false);
+  expect(localStorage.getItem("isSimpleMarkerEnabled")).toBe(null);
 
   await user.click(switchEle);
 
   expect(switchEle).toBeChecked();
+
   expect(useStore.getState().isSimpleMarkerEnabled).toBe(true);
-});
-
-test("enables simple markers when switch is clicked", async () => {
-  const user = userEvent.setup();
-
-  render(<TestComponent />);
-
-  const switchEle = screen.getByRole("switch", {
-    name: /Use simple markers/i,
-  });
-  expect(switchEle).toBeInTheDocument();
-  expect(switchEle).not.toBeChecked();
-  expect(useStore.getState().isSimpleMarkerEnabled).toBe(false);
-
-  await user.click(switchEle);
-
-  expect(switchEle).toBeChecked();
-  expect(useStore.getState().isSimpleMarkerEnabled).toBe(true);
+  expect(localStorage.getItem("isSimpleMarkerEnabled")).toBe("true");
 });
 
 test("disables legend when switch is clicked", async () => {
@@ -83,15 +108,40 @@ test("disables legend when switch is clicked", async () => {
   });
   expect(switchEle).toBeInTheDocument();
   expect(switchEle).not.toBeChecked();
+
   expect(useStore.getState().isLegendOff).toBe(false);
+  expect(localStorage.getItem("isLegendOff")).toBe(null);
 
   await user.click(switchEle);
 
   expect(switchEle).toBeChecked();
+
   expect(useStore.getState().isLegendOff).toBe(true);
+  expect(localStorage.getItem("isLegendOff")).toBe("true");
 });
 
 test("changes location range type when select is used", async () => {
+  /**
+   * Check that the select dropdown is closed.
+   */
+  const expectSelectToBeClosed = () => {
+    expect(
+      screen.queryByRole("option", {
+        name: /POI Interaction Range/i,
+      }),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole("option", {
+        name: /Wild Spawn Visibility/i,
+      }),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole("option", {
+        name: /Location Accuracy/i,
+      }),
+    ).not.toBeInTheDocument();
+  };
+
   const user = userEvent.setup();
 
   render(<TestComponent />);
@@ -101,7 +151,9 @@ test("changes location range type when select is used", async () => {
   });
   expect(selectEle).toBeInTheDocument();
   expect(selectEle).toHaveTextContent(/POI Interaction Range/i);
+
   expect(useStore.getState().myLocationRangeType).toBe("poi");
+  expect(localStorage.getItem("myLocationRangeType")).toBe(null);
 
   // Check that the select dropdown is closed
   expect(
@@ -141,26 +193,13 @@ test("changes location range type when select is used", async () => {
   // Click wild spawn option
   await user.click(optionWildSpawn);
 
-  // Check that the select dropdown is closed
-  expect(
-    screen.queryByRole("option", {
-      name: /POI Interaction Range/i,
-    }),
-  ).not.toBeInTheDocument();
-  expect(
-    screen.queryByRole("option", {
-      name: /Wild Spawn Visibility/i,
-    }),
-  ).not.toBeInTheDocument();
-  expect(
-    screen.queryByRole("option", {
-      name: /Location Accuracy/i,
-    }),
-  ).not.toBeInTheDocument();
+  expectSelectToBeClosed();
 
   // Check that select has changed to wild spawn
   expect(selectEle).toHaveTextContent(/Wild Spawn Visibility/i);
+
   expect(useStore.getState().myLocationRangeType).toBe("wild-spawn");
+  expect(localStorage.getItem("myLocationRangeType")).toBe('"wild-spawn"');
 
   // Click the label to reopen the select dropdown
   await user.click(selectEle);
@@ -171,26 +210,15 @@ test("changes location range type when select is used", async () => {
   });
   await user.click(optionLocationAccuracy);
 
-  // Check that the select dropdown is closed
-  expect(
-    screen.queryByRole("option", {
-      name: /POI Interaction Range/i,
-    }),
-  ).not.toBeInTheDocument();
-  expect(
-    screen.queryByRole("option", {
-      name: /Wild Spawn Visibility/i,
-    }),
-  ).not.toBeInTheDocument();
-  expect(
-    screen.queryByRole("option", {
-      name: /Location Accuracy/i,
-    }),
-  ).not.toBeInTheDocument();
+  expectSelectToBeClosed();
 
   // Check that select has changed to location accuracy
   expect(selectEle).toHaveTextContent(/Location Accuracy/i);
+
   expect(useStore.getState().myLocationRangeType).toBe("location-accuracy");
+  expect(localStorage.getItem("myLocationRangeType")).toBe(
+    '"location-accuracy"',
+  );
 
   // Click the label to reopen the select dropdown
   await user.click(selectEle);
@@ -201,23 +229,168 @@ test("changes location range type when select is used", async () => {
   });
   await user.click(optionPoi);
 
-  // Check that the select dropdown is closed
-  expect(
-    screen.queryByRole("option", {
-      name: /POI Interaction Range/i,
-    }),
-  ).not.toBeInTheDocument();
-  expect(
-    screen.queryByRole("option", {
-      name: /Wild Spawn Visibility/i,
-    }),
-  ).not.toBeInTheDocument();
-  expect(
-    screen.queryByRole("option", {
-      name: /Location Accuracy/i,
-    }),
-  ).not.toBeInTheDocument();
+  expectSelectToBeClosed();
 
   expect(selectEle).toHaveTextContent(/POI Interaction Range/i);
+
   expect(useStore.getState().myLocationRangeType).toBe("poi");
+  expect(localStorage.getItem("myLocationRangeType")).toBe('"poi"');
+});
+
+describe("wayfarer mode", () => {
+  it("toggles when switch is clicked", async () => {
+    const user = userEvent.setup();
+
+    render(<TestComponent />);
+
+    const switchEle = await expectWayfarerModeToTurnedOn(user);
+
+    // Click the switch to turn off the switch
+    await user.click(switchEle);
+
+    expect(switchEle).not.toBeChecked();
+    // Check that the Wayfarer section is not visible again
+    expect(
+      screen.queryByRole("group", { name: /Wayfarer Settings/i }),
+    ).not.toBeInTheDocument();
+
+    expect(useStore.getState().wayfarerMode).toBe(false);
+    expect(localStorage.getItem("wayfarerMode")).toBe("false");
+  });
+
+  test("enables hidden POIs when switch is clicked", async () => {
+    const user = userEvent.setup();
+
+    render(<TestComponent />);
+
+    await expectWayfarerModeToTurnedOn(user);
+
+    const switchEle = screen.getByRole("switch", {
+      name: /Show hidden POIs/i,
+    });
+    expect(switchEle).toBeInTheDocument();
+    expect(switchEle).not.toBeChecked();
+
+    expect(useStore.getState().modifiers.isHidden).toBe(false);
+    expect(localStorage.getItem("isHidden")).toBe(null);
+
+    await user.click(switchEle);
+
+    expect(switchEle).toBeChecked();
+
+    expect(useStore.getState().modifiers.isHidden).toBe(true);
+    expect(localStorage.getItem("isHidden")).toBe("true");
+  });
+
+  test("enables disabled & impossible power spots when switch is clicked", async () => {
+    const user = userEvent.setup();
+
+    render(<TestComponent />);
+
+    await expectWayfarerModeToTurnedOn(user);
+
+    const switchEle = screen.getByRole("switch", {
+      name: /Show disabled & impossible power spots/i,
+    });
+    expect(switchEle).toBeInTheDocument();
+    expect(switchEle).not.toBeChecked();
+
+    expect(useStore.getState().modifiers.isDisabled).toBe(false);
+    expect(localStorage.getItem("isDisabled")).toBe(null);
+
+    await user.click(switchEle);
+
+    expect(switchEle).toBeChecked();
+
+    expect(useStore.getState().modifiers.isDisabled).toBe(true);
+    expect(localStorage.getItem("isDisabled")).toBe("true");
+  });
+
+  test("enables removed POIs when switch is clicked", async () => {
+    const user = userEvent.setup();
+
+    render(<TestComponent />);
+
+    await expectWayfarerModeToTurnedOn(user);
+
+    const switchEle = screen.getByRole("switch", {
+      name: /Show removed POIs/i,
+    });
+    expect(switchEle).toBeInTheDocument();
+    expect(switchEle).not.toBeChecked();
+
+    expect(useStore.getState().modifiers.removed).toBe(false);
+    expect(localStorage.getItem("removed")).toBe(null);
+
+    await user.click(switchEle);
+
+    expect(switchEle).toBeChecked();
+
+    expect(useStore.getState().modifiers.removed).toBe(true);
+    expect(localStorage.getItem("removed")).toBe("true");
+  });
+
+  test('inverts coordinates from "Copy coords" buttons when switch is clicked', async () => {
+    const user = userEvent.setup();
+
+    render(<TestComponent />);
+
+    await expectWayfarerModeToTurnedOn(user);
+
+    const switchEle = screen.getByRole("switch", {
+      name: /Invert coordinates using the/i,
+    });
+    expect(switchEle).toBeInTheDocument();
+    expect(switchEle).not.toBeChecked();
+
+    expect(useStore.getState().invertCoords).toBe(false);
+    expect(localStorage.getItem("invertCoords")).toBe(null);
+
+    await user.click(switchEle);
+
+    expect(switchEle).toBeChecked();
+
+    expect(useStore.getState().invertCoords).toBe(true);
+    expect(localStorage.getItem("invertCoords")).toBe("true");
+  });
+});
+
+test("reset settings to default when reset button is clicked", async () => {
+  const user = userEvent.setup();
+
+  render(<TestComponent />);
+
+  await expectWayfarerModeToTurnedOn(user);
+
+  const switchEle = screen.getByRole("switch", {
+    name: /Disable animations/i,
+  });
+  expect(switchEle).toBeInTheDocument();
+  expect(switchEle).not.toBeChecked();
+
+  expect(useStore.getState().disableAnimations).toBe(false);
+  expect(localStorage.getItem("disableAnimations")).toBe(null);
+
+  await user.click(switchEle);
+
+  expect(switchEle).toBeChecked();
+
+  expect(useStore.getState().disableAnimations).toBe(true);
+  expect(localStorage.getItem("disableAnimations")).toBe("true");
+
+  const resetBtn = screen.getByRole("button", {
+    name: /Reset To Default Settings/i,
+  });
+  expect(resetBtn).toBeInTheDocument();
+
+  await user.click(resetBtn);
+
+  expect(switchEle).not.toBeChecked();
+  // Check that the Wayfarer section is not visible again
+  expect(
+    screen.queryByRole("group", { name: /Wayfarer Settings/i }),
+  ).not.toBeInTheDocument();
+
+  expect(localStorage.getItem("disableAnimations")).toBe(null);
+  expect(localStorage.getItem("wayfarerMode")).toBe(null);
 });
